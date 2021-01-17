805d64b19d14f01bfded4791451de8025e2adb0d
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-18 03:40:05 +0900

    반복문

--- Kotlin/src/chapter3/section3/WhenNoArgs.kt
+++ Kotlin/src/chapter3/section3/WhenNoArgs.kt
@@ -5,11 +5,11 @@ fun main() {
     val score = readLine()!!.toDouble()
     var grade: Char = 'F'
 
-    when(score){
-        in 90.0..100.0 -> grade = 'A'
-        in 80.0..89.9 -> grade = 'B'
-        in 70.0..79.9 -> grade = 'C'
-        else -> grade = 'F'
+    when {
+        score >= 90.0 -> grade = 'A'
+        score in 80.0..89.9 -> grade = 'B'
+        score in 70.0..79.9 -> grade = 'C'
+        score < 70.0 -> grade = 'F'
     }
 
     println("score: $score, grade: $grade")
