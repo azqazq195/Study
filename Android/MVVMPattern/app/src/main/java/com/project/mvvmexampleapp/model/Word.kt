819a12ecdd1bfccfff9b0ab67d982571d3247bd8
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-19 09:18:58 +0900

    remove

--- Android/MVVMPattern/app/src/main/java/com/project/mvvmexampleapp/model/Word.kt
+++
@@ -1,9 +0,0 @@
-package com.project.mvvmexampleapp.model
-
-import androidx.annotation.NonNull
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "word_table")
-class Word(@PrimaryKey val name: String, @NonNull val meaning: String) {
-}
