1fc1172a49e12972d0f5bbe5f03f7d97c1f58dc6
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-02-16 02:04:39 +0900

    etc

---
+++ Kotlin/src/example/Duration.kt
@@ -0,0 +1,15 @@
+package example
+
+fun main() {
+    val timeString = "1:1:800"
+    val seconds = timeString.toSecond()
+
+    println(seconds)
+}
+
+fun String.toSecond(): Int {
+    val factors = arrayOf(60000, 1000, 1)
+    var value = 0
+    this.split(":").forEachIndexed { i, s -> value += factors[i] * s.toInt() }
+    return value
+}
