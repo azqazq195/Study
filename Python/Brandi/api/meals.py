9eb7404b887aa7b38d3206b15162ad45bdcc5941
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-04-08 14:19:26 +0900

    Flask with mongo Rest api

---
+++ Python/Brandi/api/meals.py
@@ -0,0 +1,8 @@
+from mongoengine import Document, StringField, FloatField
+
+
+class Meals(Document):
+    name = StringField(required=True)
+    description = StringField()
+    price = FloatField()
+    image_url = StringField()
