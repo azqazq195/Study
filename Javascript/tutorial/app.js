0490fd3672bd36e2574ea45c312e309ad3fff5ad
SEONGHA MOON <azqazq195@gmail.com>
2022-08-25 19:44:28 +0900

    update

--- Javascript/tutorial/app.js
+++
@@ -1,8 +0,0 @@
-const dog = { type : 'dog', name: '귤', owner: { name : "문성하"}};
-
-const logging = function logging(json) {
-    console.log('[LOG]', "json");
-    console.log(json.get);
-}
-
-logging(dog);
