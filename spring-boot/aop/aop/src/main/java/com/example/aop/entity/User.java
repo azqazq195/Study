7c4249a0acd4bf65d4e7b2297171fbffca8a3993
SEONGHA MOON <azqazq195@gmail.com>
2022-08-26 23:16:41 +0900

    update

--- spring-boot/aop/aop/src/main/java/com/example/aop/entity/User.java
+++
@@ -1,2 +0,0 @@
-package com.example.aop.entity;public class User {
-}
--- spring-boot/aop/aop/src/main/java/com/example/aop/service/AuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.aop.service;
-
-public class UserService {
-    public String me() {
-        return "me";
-    }
-}
