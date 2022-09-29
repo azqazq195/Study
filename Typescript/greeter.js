8680240f66863cc0db86f6e9a7a252f46600f424
seongha.moon <seongha.moon@seonghamoons-MacBook-Pro.local>
2022-09-29 15:58:32 +0900

    node_modules 제거

--- Typescript/greeter.js
+++
@@ -1,5 +0,0 @@
-function greeter(person) {
-    return 'Hello, ' + person;
-}
-var user = 'Moseoh';
-document.body.textContent = greeter(user);
--- Typescript/greeter.ts
+++
@@ -1,6 +0,0 @@
-function greeter(person) {
-    return 'Hello, ' + person;
-}
-
-let user = 'Moseoh';
-document.body.textContent = greeter(user);
