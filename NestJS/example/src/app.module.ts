0f8d0f2ec1aa07dc29a437b3c9402f183c50b540
seongha.moon <seongha.moon@seonghamoons-MacBook-Pro.local>
2022-10-04 13:53:25 +0900

    update

--- NestJS/example/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BoardModule } from './domain/board-crud/board.module';
-import { DatabaseModule } from './domain/database/database.module';
-
-@Module({
-    imports: [DatabaseModule, BoardModule],
-    providers: [],
-})
-export class AppModule {}
