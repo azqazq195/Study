b9f0943c6d5d1a87ca70cf3983a5c90d209de048
seongha.moon <azqazq195@gmail.com>
2022-10-25 18:06:04 +0900

    feat: Coffee controller, service, entity 추가

--- NestJS/nestjs-course/src/app.module.ts
+++ NestJS/nestjs-course/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeeController } from './domain/coffee/controller/coffee.controller';
+import { CoffeeService } from './domain/coffee/service/coffee.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CoffeeController],
+  providers: [AppService, CoffeeService],
 })
 export class AppModule {}
---
+++ NestJS/nestjs-course/src/domain/coffee/controller/coffee.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeeService } from '../service/coffee.service';
+
+@Controller('coffees')
+export class CoffeeController {
+  constructor(private readonly coffeeService: CoffeeService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    // return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body) {
+    return this.coffeeService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeeService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
