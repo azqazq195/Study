9a4960f4116c8aa59f382ef930bea4bb191d255e
seongha.moon <azqazq195@gmail.com>
2022-11-09 16:24:50 +0900

    feat: database module 추가

--- NestJS/nestjs-course/src/app.module.ts
+++ NestJS/nestjs-course/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './domain/coffee/coffee.module';
 import { GlobalValidationPipe } from './shared/pipe/global-validation.pipe';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [CoffeeModule, GlobalValidationPipe],
+  imports: [GlobalValidationPipe, DatabaseModule, CoffeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ NestJS/nestjs-course/src/shared/database/database.module.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+
+      logging: true,
+      connectTimeout: 3000,
+    }),
+  ],
+})
+export class DatabaseModule {}
