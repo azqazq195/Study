8c8c37959d4bc92fafc2015c16556254fd6c426f
seongha.moon <azqazq195@gmail.com>
2022-12-01 13:50:40 +0900

    spring-boot kotlin update

--- spring-boot-kotlin/converter/src/main/kotlin/com/example/converter/member/entity/Member.kt
+++ spring-boot-kotlin/converter/src/main/kotlin/com/example/converter/member/entity/Member.kt
@@ -1,4 +1,19 @@
-package com.example.converter.member
+package com.example.converter.member.entity
 
-class Member {
-}
+import com.example.converter.member.converter.AES256Converter
+import com.example.converter.member.converter.BooleanToTinyintConverter
+import jakarta.persistence.*
+
+@Entity
+data class Member(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private var id: Long?,
+
+    val name: String,
+
+    @Convert(converter = AES256Converter::class)
+    val regNo: String,
+
+    @Convert(converter = BooleanToTinyintConverter::class)
+    val deleted: Boolean = false
+)
--- spring-boot-kotlin/converter/src/main/kotlin/com/example/converter/member/repository/MemberRepository.kt
+++ spring-boot-kotlin/converter/src/main/kotlin/com/example/converter/member/repository/MemberRepository.kt
@@ -1,4 +1,10 @@
 package com.example.converter.member.repository
 
-class MemberRepository {
+import com.example.converter.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long>{
+    override fun findAll(): MutableList<Member>
 }
