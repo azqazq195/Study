212160fe573b0b26dda47c5e188d335fae52affd
seongha.moon <azqazq195@gmail.com>
2022-12-05 19:30:30 +0900

    update

--- NestJS/example/src/domain/user/user/entity/enum/role.enum.ts
+++ NestJS/example/src/domain/user/user/entity/enum/role.enum.ts
@@ -2,21 +2,17 @@
 // * 1. DB 조회 > value 가 int 경우 조회가 번거롭거나 comment 필수
 // * 2. 개발 편의성 > enum 값 순서 변경, 추가 등에 따른 int 값 변동
 // * 3. int 값 비교나 string 값 비교의 성능상 차이가 크지 않음.
+
+// * 권한 레벨
+// * 최고관리자 > 관리자 > 팀장 > 직원
 export enum Role {
-  // * MASTER 권한
-  최고관리자 = 'MASTER_ADMIN',
+  // * MASTER 권한으로 관리자를 관리한다.
+  // * 타부서에 부여하지 않는다.
+  최고관리자 = 'MASTER',
 
-  // * 오늘의 꽃 권한
-  오늘의꽃_관리자 = 'OKKOT_ADMIN',
-  오늘의꽃_임직원 = 'OKKOT_EMPLOYEE',
-
-  // * 오프라인 매장 권한
-  오프라인매장_관리자 = 'OFFLINE_ADMIN',
-  오프라인매장_임직원 = 'OFFLINE_EMPLOYEE',
-
-  // * 외부업체 고객 권한
-  외부업체_관리자 = 'INDUSTRY_ADMIN',
-  외부업체_임직원 = 'INDUSTRY_EMPLOYEE',
+  관리자 = 'ADMIN',
+  팀장 = 'MANAGER',
+  직원 = 'EMPLOYEE',
 
   // * 일반 고객 권한
   일반고객 = 'CUSTOMER',
