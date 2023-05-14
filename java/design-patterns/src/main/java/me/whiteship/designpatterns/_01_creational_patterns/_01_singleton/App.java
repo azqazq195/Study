917f99bf86f0f28612c97524a137e3c205517278
moseoh <azqazq195@gmail.com>
2023-05-15 06:56:07 +0900

    refactor: reflection 에제 추가

--- java/design-patterns/src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ java/design-patterns/src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -1,22 +1,37 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings5 settings = Settings5.INSTANCE;
+    public static void main(String[] args) throws Exception {
+        reflection();
+        serialize();
+    }
 
-        Settings5 settings1 = null;
+    public static void reflection() throws Exception {
+        Settings4 settings1 = Settings4.getInstance();
+
+        Constructor<Settings4> constructor = Settings4.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings4 settings2 = constructor.newInstance();
+
+        System.out.println(settings1 == settings2);
+    }
+
+    public static void serialize() throws Exception {
+        Settings4 settings1 = Settings4.getInstance();
+
+        Settings4 settings2;
         try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
-            out.writeObject(settings);
+            out.writeObject(settings1);
         }
 
         try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings1 = (Settings5) in.readObject();
+            settings2 = (Settings4) in.readObject();
         }
 
-        System.out.println(settings == settings1);
+        System.out.println(settings1 == settings2);
     }
-
 }
