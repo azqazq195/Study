4375f081f6138c9cd0bd9381ab3e0ce5b7e0ef94
moseoh <azqazq195@gmail.com>
2023-05-20 08:46:41 +0900

    feat: 메서드 추가

--- spring-boot/jwt/src/main/java/com/example/jwt/user/domain/repository/UserRepository.java
+++ spring-boot/jwt/src/main/java/com/example/jwt/user/domain/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.example.jwt.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
