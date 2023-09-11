178de5a4bf9bb3dcdbd254fbc6d4cc950b4100fb
moseoh <azqazq195@gmail.com>
2023-09-11 18:41:57 +0900

    feat: hash table

---
+++ java/src/main/java/hash_table/HashTable.java
@@ -0,0 +1,17 @@
+package hash_table;
+
+public interface HashTable<K, V> {
+
+    int hash(Object key);
+
+    void put(K key, V value);
+
+    void remove(K key);
+
+    V get(K key);
+
+    boolean isEmpty();
+
+    @Override
+    String toString();
+}
