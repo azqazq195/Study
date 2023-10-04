b41395709cd4e71550f9d3397429a0ccf9396c1a
moseoh <azqazq195@gmail.com>
2023-10-04 18:26:46 +0900

    init

---
+++ java/techlabs/src/main/kotlin/com/moseoh/techlabs/TechlabsApplication.kt
@@ -0,0 +1,11 @@
+package com.moseoh.techlabs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechlabsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TechlabsApplication>(*args)
+}
